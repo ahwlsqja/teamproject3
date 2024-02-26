b1de98b99fda64476fbc3ce5a6f60620e5416d8d
slianzg <slian201248@gmail.com>
2024-02-26 19:06:33 +0900

    fix: 오타수정/ 스키마 enum 값 uppercase로 통일

--- src/middlewares/error-handling.middleware.js
+++ src/middlewares/error-handling.middleware.js
@@ -1,4 +1,4 @@
-export default function (err, req, res, next) { 
-    console.error(err);
-    res.status(500).json({ message: "서비 내부에서 에러가 발생했습니다."});
+export default function (err, req, res, next) {
+  console.error(err);
+  res.status(500).json({ message: "서버 내부에서 에러가 발생했습니다." });
 }
